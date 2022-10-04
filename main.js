b7f89099969b1f8bef60c2be6055991109bfc99e
Carina Ogbat <carinawrightogbat@gmail.com>
2022-10-03 20:43:07 -0700

    was missing .class on query selector and quotes on click, code now working

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+
+
+function changeColor() {
+    const colorChanging = document.querySelectorAll('.color-change');
+    for (const item of colorChanging){
+        item.classList.add('red');
+    }
+}
+
+document.querySelector('.color-changer').addEventListener('click', changeColor);
+
+// function changeColor() {
+//     const colorChangeEls = document.querySelectorAll('.color-change');
+  
+//     for (const el of colorChangeEls) {
+//       el.classList.add('red');
+//     }
+//   }
+  
+
+//   document.querySelector('.color-changer').addEventListener('click', changeColor);
