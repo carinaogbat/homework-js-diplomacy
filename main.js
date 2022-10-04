2e6cee78efdb3e8bce2e049329da788fc8b870f9
Carina Ogbat <carinawrightogbat@gmail.com>
2022-10-03 21:23:21 -0700

    my code for number check still has bug

--- main.js
+++ main.js
@@ -22,3 +22,33 @@ document.querySelector('.color-changer').addEventListener('click', changeColor);
   
 
 //   document.querySelector('.color-changer').addEventListener('click', changeColor);
+
+function validateInput(evt){
+    evt.preventDefault();
+    const formButton = document.querySelector('#number-input');
+    const userNumber = Number(numberInput.value);
+    const feedbackForm = document.querySelector('#formFeedback');
+    if (isNAN(userNumber) || userNumber >= 10) {
+    formFeedback.innerText = 'Please enter a smaller number';
+    }  else {
+        formFeedback.innerText = 'You are good to go!';
+    } 
+    } 
+
+    document.querySelector('.number-form').addEventListener('submit', validateInput);
+
+// function validateNumber(evt) {
+//     evt.preventDefault();
+  
+//     const numberInput = document.querySelector('#number-input');
+//     const userNum = Number(numberInput.value); // typecast to num
+  
+//     const formFeedback = document.querySelector('#formFeedback');
+//     if (isNaN(userNum) || userNum >= 10) {
+//       formFeedback.innerText = 'Please enter a smaller number';
+//     } else {
+//       formFeedback.innerText = 'You are good to go!';
+//     }
+//   }
+  
+//   document.querySelector('.number-form').addEventListener('submit', validateNumber);
